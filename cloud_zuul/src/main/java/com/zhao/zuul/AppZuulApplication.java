2fb1a13f9a336b5c7468eea1882ece7133e32e1f
zhaoyk <zyk_1778414235@163.com>
2025-08-30 22:34:32 +0800

    重新编译为maven父子springcloud项目

---
+++ cloud_zuul/src/main/java/com/zhao/zuul/AppZuulApplication.java
@@ -0,0 +1,15 @@
+package com.zhao.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class AppZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppZuulApplication.class, args);
+    }
+}
