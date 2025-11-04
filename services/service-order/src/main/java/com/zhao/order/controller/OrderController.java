65260bdcaea59c252025f5a1a56c19b9a78c37d3
yk_repository <50788116+yk-repository@users.noreply.github.com>
2025-11-04 13:19:27 +0800

    配置中心的基本使用和@RefreshScope配置刷新的基本使用，看禁用不需要模块的配置中心

--- services/service-order/src/main/java/com/zhao/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/zhao/order/controller/OrderController.java
@@ -2,16 +2,25 @@ package com.zhao.order.controller;
 
 import com.zhao.order.bean.Order;
 import com.zhao.order.service.OrderService;
-import com.zhao.product.bean.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope // 激活配置属性的刷新功能
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+
+    // 获取配置中心的内容
+    @Value("${order.timeout}")
+    private String timeout;
+
+    @Value("${order.auto-confirm}")
+    private String autoConfirm;
 
     @Autowired
     private OrderService orderService;
@@ -20,7 +29,13 @@ public class OrderController {
     @GetMapping("/create")
     public Order createOrder(@RequestParam("productId") Long productId,
                              @RequestParam("userId") Long userId) {
-        Order order = orderService.createOrder(productId, userId);
-        return order;
+        return orderService.createOrder(productId, userId);
     }
+
+    // 获取配置中心的配置文件
+    @GetMapping("/config")
+    public String getConfig() {
+        return "service-order.properties: timeout=" + timeout + ", autoConfirm=" + autoConfirm;
+    }
+
 }
