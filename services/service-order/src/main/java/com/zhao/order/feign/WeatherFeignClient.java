cb3fb20cbadb07db865bcc5495e13dc81d34b582
yk_repository <50788116+yk-repository@users.noreply.github.com>
2025-11-04 23:33:33 +0800

    feign的声明式远程调用服务实现，模拟外部API的远程调用

--- services/service-order/src/main/java/com/zhao/order/feign/WeatherFeignClient.java
+++ services/service-order/src/main/java/com/zhao/order/feign/WeatherFeignClient.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+// 指定了服务名，url 是服务的地址
 @FeignClient(value = "service-weather", url="http://aliv8.data.moji.com")
 public interface WeatherFeignClient {
 
