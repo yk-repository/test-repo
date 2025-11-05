c4ef33f042484bb309fc7ac340332920e2fb86a2
yk_repository <50788116+yk-repository@users.noreply.github.com>
2025-11-05 11:13:56 +0800

    feign的日志配置

--- services/service-order/src/main/java/com/zhao/order/config/OrderRestTemplateServiceConfig.java
+++ services/service-order/src/main/java/com/zhao/order/config/OrderRestTemplateServiceConfig.java
@@ -1,7 +1,6 @@
 package com.zhao.order.config;
 
 import feign.Logger;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
